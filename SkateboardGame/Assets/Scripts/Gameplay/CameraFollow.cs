799f5db022fbb4d5ed3607ae34a5e251a308f512
Awesomecake <aaronodellbush@gmail.com>
2025-01-16 00:45:25 -0500

    added jump

---
+++ SkateboardGame/Assets/Scripts/Gameplay/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] private GameObject objectToFollow;
+    [SerializeField] private Camera cameraToMove;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraToMove.transform.position = new Vector3(objectToFollow.transform.position.x,0,-10);
+    }
+}
--- SkateboardGame/Assets/Scripts/Gameplay/DriveSkateboard.cs
+++ SkateboardGame/Assets/Scripts/Gameplay/DriveSkateboard.cs
@@ -12,24 +12,47 @@ public class DriveSkateboard : MonoBehaviour
     [SerializeField] private float rotationSpeed;
 
     private float moveInput;
+    private int gravityMult = 1;
+
+    //booleans
+    private bool canJump = true;
 
     private void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }    
+    }
+
+    public virtual void OnCollisionEnter(Collision c)
+    {
+        Debug.Log(c.gameObject);
     }
 
     private void FixedUpdate()
     {
-        rightTireRB.AddTorque(-moveInput * speed * Time.fixedDeltaTime);
-        leftTireRB.AddTorque(-moveInput * speed * Time.fixedDeltaTime);
+        rightTireRB.AddTorque(-moveInput * gravityMult * speed * Time.fixedDeltaTime);
+        leftTireRB.AddTorque(-moveInput * gravityMult * speed * Time.fixedDeltaTime);
 
         skateboardRB.AddTorque(moveInput * rotationSpeed * Time.fixedDeltaTime);
     }
 
     public void FlipGravity()
     {
+        gravityMult *= -1;
         leftTireRB.gravityScale *= -1;
         rightTireRB.gravityScale *= -1;
         skateboardRB.gravityScale *= -1;
+    }
+
+    public void Jump()
+    {
+        skateboardRB.velocity = Vector3.zero;
+        rightTireRB.velocity = Vector3.zero;
+        leftTireRB.velocity = Vector3.zero;
+        skateboardRB.AddRelativeForce(new Vector2(0, 1000));
     }
 }
