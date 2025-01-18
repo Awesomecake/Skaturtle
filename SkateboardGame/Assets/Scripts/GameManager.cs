14d367eae9a4be4cc7816cbb8b912c1639b37cad
Patrick Emmons (RIT Student) <ple5024@rit.edu>
2025-01-18 14:23:10 -0500

    pushed initial gamemanager stuff

---
+++ SkateboardGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public int tempScore = 0;
+    public int score = 0;
+    int highScore = 0;
+    public int scoreMultiplier = 1;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(GameManager).ToString());
+                    _instance = singleton.AddComponent<GameManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
