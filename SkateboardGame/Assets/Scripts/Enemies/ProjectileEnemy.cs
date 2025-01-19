daafae81569cd3ea9c403e11a33edf3027656007
Ashank Rajendran <ashankrajendran@gmail.com>
2025-01-19 12:47:22 -0500

    Adjusted Projectile Enemy behavior

--- SkateboardGame/Assets/Scripts/Enemies/ProjectileEnemy.cs
+++ SkateboardGame/Assets/Scripts/Enemies/ProjectileEnemy.cs
@@ -8,6 +8,7 @@ public class ProjectileEnemy : Enemy
     [SerializeField] Transform target;
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float fireRateInSeconds;
+    [SerializeField] float fireRange;
 
     private void Start()
     {
@@ -26,8 +27,13 @@ public class ProjectileEnemy : Enemy
 
     void SpawnProjectile()
     {
-        Vector2 projectileDirection = target.position - transform.position;
-        Projectile newProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity).GetComponent<Projectile>();
-        newProjectile.FireProjectile(projectileDirection);
+        Vector2 projectileDistance = target.position - transform.position;
+
+        if (projectileDistance.magnitude < fireRange)
+        {
+            Vector2 projectileDirection = projectileDistance.normalized;
+            Projectile newProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity).GetComponent<Projectile>();
+            newProjectile.FireProjectile(projectileDirection);
+        }
     }
 }
