4e9ad1ddf8c76f448a9e687886b4374b9556002e
river-t94 <kdw4401@g.rit.edu>
2025-01-23 16:32:47 -0500

    added score display

--- SkateboardGame/Assets/Scripts/UI/ScoreUI.cs
+++ SkateboardGame/Assets/Scripts/UI/ScoreUI.cs
@@ -2,25 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreUI : MonoBehaviour
 {
-    public int totalScore;
-    public int multiplier;
-    //[SerializeField] 
-    //private TMP_TEXT scoreText;
+    public TextMeshProUGUI displayedScore; 
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        totalScore = 12; //where to set this 
+        displayedScore.text = GameManager.Instance.score.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-          // if 
+        displayedScore.text = GameManager.Instance.score.ToString();
+        // if 
         // if trick update score 
         // if shell break update multipler 
     }
